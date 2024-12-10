ff5e1f814ec75683419e0e75bd2abe20edb3910d
Maksim Shabunin <maksim.shabunin@gmail.com>
2024-12-10 18:54:27 +0300

    fixup! Add custom action and unified Linux build

--- scripts/runner.py
+++ scripts/runner.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run gtest executable in GHA context')
     parser.add_argument('exe', help='Executable to run')
     parser.add_argument('--log', type=Path, help='Path to store XML log')
-    parser.add_argument('--timeout', type=int, default=60, help='Timeout in minutes')
+    parser.add_argument('--timeout', type=int, default=10, help='Timeout in minutes')
     parser.add_argument('--wrap', default='', help="Wrapper command")
     parser.add_argument('--workdir', default='.', help="Change working dir")
 
@@ -23,12 +23,14 @@ if __name__ == "__main__":
     full_exe = args.wrap.split() + [args.exe] + options + other
     print("Run: {}".format(full_exe), flush=True)
     try:
-        res = subprocess.run(full_exe, timeout=args.timeout, check=True, cwd=args.workdir)
+        res = subprocess.run(full_exe, timeout=args.timeout * 60, check=True, cwd=args.workdir)
         status = res.returncode
     except Exception as err:
+        print("", flush=True)
         print("::error::{}".format(err), flush=True)
         status = -1
 
+    print("", flush=True)
     print("Exit status: {}".format(status), flush=True)
     print("::endgroup::", flush=True)
 
